e6d944f6ef04acdafdb9e5210acd39b70784a91a
abhipoo <abhishek@letstranzact.com>
2020-02-27 20:41:13 +0530

    Deleted users app

--- simpleerp/simpleerp/settings.py
+++ simpleerp/simpleerp/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'inventory.apps.InventoryConfig',
-    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
